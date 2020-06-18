b4056ee40ecb055ca433c9f33a3fbc925fae1888
HughG-50 <hugh.greethead@gmail.com>
2020-06-19 09:53:43 +1000

    Updating board

--- tictactoe.js
+++ tictactoe.js
@@ -1,22 +1,21 @@
-var board = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
+let board = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
 let currentPlayer = "X"
 
 const squares = Array.from(document.querySelectorAll('td'));
-
-console.log(squares)
-
-console.log(squares[2])
 
 squares.forEach(square => {
     square.addEventListener("click", () => {
         if (square.innerHTML === '') {
             square.innerHTML = currentPlayer
+            board[parseInt(square.id)] = currentPlayer
             if (currentPlayer === "X") {
                 currentPlayer = "O"
             }
             else if (currentPlayer === "O") {
                 currentPlayer = "X"
             }
+            console.log(square.id)
         }
     })
 })
+
