13e0a40316242656b468ff6ef7f7f4e3521250b2
Morgan <morgan.batterham@gmail.com>
2020-06-18 16:36:09 +1000

    styling

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,9 @@
-let board = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined
+let board = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
 
+let square = document.querySelectorAll("td")
 
+square.addEventListener("click", clickSquare())
+
+function clickSquare() {
+    console.log("square clicked")
+}
