e874853b1366a7cd55f66cd09614b6cee6002386
Morgan <morgan.batterham@gmail.com>
2020-06-18 15:23:04 +1000

    styling work and changed board structure

--- tictactoe.js
+++ tictactoe.js
@@ -1,17 +1,3 @@
-const beginBoard = [
-    [undefined, undefined, undefined],
-    [undefined, undefined, undefined],
-    [undefined, undefined, undefined]
-    ]
+let board = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined
 
-let board = beginBoard
 
-let cell1 = board[0][0]
-let cell2 = board[0][1]
-let cell3 = board[0][2]
-let cell4 = board[1][0]
-let cell5 = board[1][1]
-let cell6 = board[1][2]
-let cell7 = board[2][0]
-let cell8 = board[2][1]
-let cell9 = board[2][2]
