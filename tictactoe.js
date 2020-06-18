45029dd57e74b7c06135e4d41293ef6517879045
Morgan <morgan.batterham@gmail.com>
2020-06-19 09:45:26 +1000

    changed board to global variable

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,4 @@
-let board = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
+var board = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
 let currentPlayer = "X"
 
 const squares = Array.from(document.querySelectorAll('td'));
@@ -9,12 +9,14 @@ console.log(squares[2])
 
 squares.forEach(square => {
     square.addEventListener("click", () => {
-        square.innerHTML = currentPlayer
-        if (currentPlayer === "X") {
-            currentPlayer = "O"
-        }
-        else if (currentPlayer === "O") {
-            currentPlayer = "X"
+        if (square.innerHTML === '') {
+            square.innerHTML = currentPlayer
+            if (currentPlayer === "X") {
+                currentPlayer = "O"
+            }
+            else if (currentPlayer === "O") {
+                currentPlayer = "X"
+            }
         }
     })
 })
