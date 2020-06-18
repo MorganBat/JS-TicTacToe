ec65809e214f88efe551924394d6cac112b3cbeb
Morgan <morgan.batterham@gmail.com>
2020-06-19 09:30:06 +1000

    add X to page

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,14 @@
 let board = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
 let currentPlayer = "X"
 
-const squares = document.querySelectorAll("td")
+const squares = Array.from(document.querySelectorAll('td'));
+
+console.log(squares)
+
+console.log(squares[2])
 
 squares.forEach(square => {
     square.addEventListener("click", () => {
-        console.log(square)
+        square.innerHTML = currentPlayer
     })
 })
