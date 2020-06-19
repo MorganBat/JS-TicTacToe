c9dd81c931272b40891ebcf649f37dd816c929f2
HughG-50 <hugh.greethead@gmail.com>
2020-06-19 10:03:27 +1000

    Board state

--- tictactoe.js
+++ tictactoe.js
@@ -16,6 +16,26 @@ squares.forEach(square => {
             }
             console.log(square.id)
         }
+
     })
 })
 
+function isGameWon(boardState) {
+    const winningLines = [
+        // Rows
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        // Columns
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        // Diagonals
+        [0, 4, 8],
+        [2, 4, 6]
+        ];
+
+    if (boardState[winningLines[0]] === boardState[winningLines[1]] && boardState[winningLines[1]] === boardState[winningLines[2]]){
+        console.log(`${currentPlayer} has won!`)
+    }
+}
